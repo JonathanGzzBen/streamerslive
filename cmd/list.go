18709437c07761578f1427d543253dda7ca84eed
Jonathan Gzz <jonathangzzben@gmail.com>
2021-01-06 14:57:19 -0600

    Add list command

---
+++ cmd/list.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/JonathanGzzBen/streamerslive/pkg/storage"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "displays saved streaming channels",
+	Run: func(cmd *cobra.Command, args []string) {
+		cURLs, err := storage.ChannelURLs()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, `No channels stored. Use command "add" to store a channel`)
+			return
+		}
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"Channel Name", "Stream Title", "Stream URL"})
+		cchan := channelsChan(cURLs...)
+		for c := range cchan {
+			table.Append([]string{c.Name, c.Stream.Title, c.Stream.URL})
+		}
+		table.Render()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
